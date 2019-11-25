1ff8b20d8ee1eb6721fe671dac1f24820ef3e01c
aluno <aluno>
2019-11-25 16:57:48 -0300

    Codigo do projeto de treinamento.

---
+++ nodeJs/nodeEventos/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var load = require('express-load');
+var path = require('path');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.listen(3000, function () {
+  console.log("Aplicação no ar.");
+});
+
+module.exports = app;
